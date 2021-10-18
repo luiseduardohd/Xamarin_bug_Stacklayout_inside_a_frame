bc4e7489b97f39e9c224fea9a4b0a5fc7e3b5daf
Luis Hernandez <luiseduardohd@users.noreply.github.com>
2021-10-17 19:06:45 -0500

    init commit

---
+++ TestBug/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TestBug.Services;
+using TestBug.Views;
+
+namespace TestBug
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            //DependencyService.Register<MockDataStore>();
+            //MainPage = new AppShell();
+            MainPage = new BugTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TestBug/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TestBug.ViewModels;
+
+namespace TestBug.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
